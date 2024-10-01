47c7a4ce816527d40f0109625ca90a8568a32acf
Ayahviramempesonah <bayusatriapurnomo@gmail.com>
2024-10-02 01:21:02 +0700

    gas tipis

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import Image from 'next/image';
+import quotes from './quotes.jsx';
+import Copyright from './copyRigt.jsx';
+import FancyText from './FancyText.jsx';
+import InspirationGenerator from './InspirationGenerator.jsx';
+
 
 export default function Home() {
   return (
@@ -8,9 +13,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
-        <div>
-          <p>jembiutljah</p>
-        </div>
+        
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
         
 
@@ -20,7 +23,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            {' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
--- app/quotes.jsx
+++ app/quotes.jsx
@@ -1,4 +1,4 @@
-export default [
+export default quotes=[
   "Jangan biarkan hari kemarin menyita terlalu banyak waktu di hari ini.” — Will Rogers",
   "Ambisi adalah menyusun tangga menuju langit.",
   "Kegembiraan yang dibagi adalah kegembiraan yang berlipat ganda.",
